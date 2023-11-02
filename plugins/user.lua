b9193b03fe1dc2b75e9d6de64ea1d0ddf00825f0
alberte <niklas.renken@freun.de>
2023-11-02 19:03:40 +0100

    several changes

--- plugins/user.lua
+++ plugins/user.lua
@@ -11,15 +11,40 @@ return {
   -- },
   --
   {
-    "max397574/better-escape.nvim", config = function() 
-      require("better_escape").setup()
-    end,
-  }, {
-    "simrat39/rust-tools.nvim", {
-      "williamboman/mason-lspconfig.nvim", 
+    "max397574/better-escape.nvim",
+    config = function() require("better_escape").setup() end,
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    {
+      "williamboman/mason-lspconfig.nvim",
       opts = {
-        ensure_installed = {"rust_analyzer"},
+        ensure_installed = { "rust_analyzer" },
+      },
+    },
+  },
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {},
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
       }
-    }
-  }
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  },
 }
